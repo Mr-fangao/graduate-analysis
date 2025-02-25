4ad35d37398fd614fbd7b439ac13d5f18c77d6f8
liqifeng <Mr.undefine@protonmail.com>
2025-02-25 15:21:15 +0800

    时空分析

--- src/views/Home/components/Analysis/index.vue
+++ src/views/Home/components/Analysis/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: liqifeng
  * @Date: 2024-08-26 09:37:14
- * @LastEditors: Mr-fangao Mr.undefine@protonmail.com
- * @LastEditTime: 2025-02-24 22:17:26
+ * @LastEditors: liqifeng Mr.undefine@protonmail.com
+ * @LastEditTime: 2025-02-25 15:21:10
  * @Description:
 -->
 <script setup>
@@ -35,6 +35,8 @@ import ScaleBar from "@arcgis/core/widgets/ScaleBar";
 import Compass from "@arcgis/core/widgets/Compass";
 import ScaleRangeSlider from "@arcgis/core/widgets/ScaleRangeSlider";
 import Zoom from "@arcgis/core/widgets/Zoom";
+import { fi } from "element-plus/es/locales.mjs";
+
 esriConfig.locale = "zh";
 const { proxy } = getCurrentInstance();
 const loginStore = useLoginStore();
@@ -48,24 +50,15 @@ function leftPanelClick() {
   rightCollapse.value = !rightCollapse.value;
 }
 const formState = reactive({
-  rate: "1",
-  direction: "landscape",
-  top: "",
-  left: "",
-  bottom: "",
-  right: "",
-  size: "A4",
-  dpi: 100,
-  northsize: null,
-  north: null,
-  name: "",
-  subname: "",
-  scalebarsize: "",
-  scalebar: "",
-  fillcolor: "",
-  filename: "",
+  type: "聚类分析",
+  timerange: "",
+  data: "就业地",
+  field: "就业数量",
 });
-const plainOptions = ref('2024毕业生生源地分析图');
+const fieldList = ref(["就业数量", "生源数量"]);
+const typeList = ref(["标准差椭圆分析", "聚类分析", "空间自相关分析", "核密度分析", "冷热点分析"]);
+const dataList = ref(["生源地", "就业地"]);
+const plainOptions = ref([]);
 const checkedList = [
   {
     label: "2024毕业生生源地分析图",
@@ -73,7 +66,7 @@ const checkedList = [
   },
   {
     label: "2023毕业生生源地分析图",
-
+    value: "2022",
   },
   {
     label: "2022毕业生生源地分析图",
@@ -88,23 +81,8 @@ const showLayerList = ref([]);
 // const formState.size = ref('A4')
 // const formState.direction = ref('portrait')
 
-const paperDimensions = {
-  A3: { width: 297, height: 420 },
-  A4: { width: 210, height: 297 },
-  A5: { width: 148, height: 210 },
-  B3: { width: 353, height: 500 },
-  B4: { width: 250, height: 353 },
-  Letter: { width: 216, height: 279 },
-  Legal: { width: 216, height: 356 },
-};
-const paperSizes = Object.keys(paperDimensions);
-const onSubmit = () => {
-  console.log("submit!", toRaw(formState));
-};
+
 function initmap() {
-  // const map = new Map({
-  //     basemap: 'streets-night-vector',
-  // });
   const webMap = new WebMap({
     portalItem: {
       id: "1211472e6c3d45fe95793af7ad7d3d89", // 替换为实际的 portalId
@@ -137,108 +115,7 @@ function initmap() {
     // view.ui.add(scaleBar, "top-left");
   });
 }
-const mmToPixels = (mm) => Math.round((mm * formState.dpi) / 25.4);
-const getPrintSize = () => {
-  const dimensions = paperDimensions[formState.size];
-  const isLandscape = formState.direction === "landscape";
-  return {
-    width: mmToPixels(isLandscape ? dimensions.height : dimensions.width),
-    height: mmToPixels(isLandscape ? dimensions.width : dimensions.height),
-  };
-};
 
-const processExport = async (callback) => {
-  const { width, height } = getPrintSize();
-  const contentWidth = width - formState.left - formState.right;
-  const contentHeight = height - formState.top - formState.bottom;
-  // 第一步：获取地图截图
-  const screenshot = await view.takeScreenshot({
-    width: contentWidth,
-    height: contentHeight,
-    format: "png",
-    quality: 100,
-  });
-  // 第二步：使用html2canvas合成其他元素
-  const printArea = document.getElementById("printArea");
-  const printMap = printArea.querySelector("#printMap");
-  // 更新打印预览地图尺寸
-  printMap.style.width = `${contentWidth}px`;
-  printMap.style.height = `${contentHeight}px`;
-  // 将截图插入打印区域
-  const img = new Image();
-  img.src = screenshot.dataUrl;
-  printMap.innerHTML = "";
-  printMap.appendChild(img);
-  // 合成整个打印区域
-  const canvas = await html2canvas(printArea, {
-    useCORS: true,
-    scale: formState.rate,
-    logging: false,
-    width: contentWidth,
-    height: contentHeight,
-  });
-  const finalCanvas = document.createElement("canvas");
-  finalCanvas.width = width;
-  finalCanvas.height = height;
-  const context = finalCanvas.getContext("2d");
-  context.fillStyle = "#FFFFFF";
-  context.fillRect(0, 0, width, height);
-  context.drawImage(canvas, formState.left, formState.top);
-  if (formState.name) {
-    // 添加图名文本
-    const title = formState.name; // 替换为实际的图名
-    const titleFontSize = 24; // 字体大小
-    const titleColor = "#FFFFFF"; // 字体颜色
-    const titleX = width / 2; // 水平居中
-    const titleY = 50; // 距离顶部50px
-    context.font = `${titleFontSize}px Arial`; // 设置字体
-    context.fillStyle = titleColor; // 设置颜色
-    context.textAlign = "center"; // 水平居中
-    context.textBaseline = "top"; // 垂直对齐方式
-    context.fillText(title, titleX, titleY); // 绘制文本
-  }
-  console.log("Canvas Size:", {
-    width: finalCanvas.width,
-    height: finalCanvas.height,
-  });
-  callback(finalCanvas);
-};
-
-// 导出为图片
-const exportImage = () => {
-  processExport((canvas) => {
-    const link = document.createElement("a");
-    let name = `map_${formState.size}_${formState.direction}.png`;
-    if (formState.filename) {
-      name = formState.filename + ".png";
-    }
-    link.download = name;
-    link.href = canvas.toDataURL("image/png");
-    link.click();
-  });
-};
-
-// 导出为PDF
-const exportPDF = () => {
-  processExport((canvas) => {
-    const pdf = new jsPDF({
-      orientation: formState.direction,
-      unit: "mm",
-      format: formState.size.toLowerCase(),
-    });
-
-    const imgData = canvas.toDataURL("image/png");
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = pdf.internal.pageSize.getHeight();
-
-    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-    let name = `map_${formState.size}_${formState.direction}.pdf`;
-    if (formState.filename) {
-      name = formState.filename + ".pdf";
-    }
-    pdf.save(name);
-  });
-};
 function toolClick() {
   activeKey.value = !activeKey.value;
 }
@@ -307,7 +184,7 @@ function close() {
   activeKey.value = false;
 }
 onMounted(() => {
-  if (MenuIndex.value == "/home/mapping") {
+  if (MenuIndex.value == "/home/analysis") {
     initmap();
     // setTimeout(() => {
     //     paperSizes.value = Object.keys(paperDimensions)
@@ -318,122 +195,56 @@ onMounted(() => {
 <template>
   <div class="Mapping">
     <div id="mappingview" ref="mapView"></div>
-    <div id="printArea" class="print-area">
-      <div class="map-wrapper">
-        <div id="printMap"></div>
+    <div :class="['FlyPanel']">
+      <!-- <div :class="['collapse', leftCollapse ? 'active' : '']" @click="leftPanelClick"></div> -->
+      <div class="title">
+        <span>时空分析</span>
+        <img src="./assets/indent.svg" class="close" />
       </div>
-      <!-- <div class="map-overlay">
-                <div class="map-title">我的地图</div>
-            </div> -->
-    </div>
-    <div :class="['LeftPanel', leftCollapse ? 'closed' : 'opened']">
-      <div :class="['collapse', leftCollapse ? 'active' : '']" @click="leftPanelClick"></div>
-      <div class="LabelContent" style="margin-top: 2vh">
-        <span class="title-ellipsis">版式布局</span>
+      <div class="container">
+        <div class="content">
+          <div class="LabelContent" style="margin-top: 2vh">
+            <span class="title-ellipsis">分析类型选择</span>
+          </div>
+          <div class="l1">
+            <a-form :model="formState">
+              <a-form-item label="分析类型">
+                <a-select v-model:value="formState.type" style="width: 100%;" placeholder="">
+                  <a-select-option v-for="(item, index) in typeList" :value="item" :key="index">{{ item
+                  }}</a-select-option>
+                </a-select>
+              </a-form-item>
+              <a-form-item label="分析对象">
+                <a-select v-model:value="formState.data" style="width: 100%;" placeholder="">
+                  <a-select-option v-for="(item, index) in dataList" :value="item" :key="index">{{ item
+                  }}</a-select-option>
+                </a-select>
+              </a-form-item>
+              <a-form-item label="分析字段">
+                <a-select v-model:value="formState.field" style="width: 100%;" placeholder="">
+                  <a-select-option v-for="(item, index) in fieldList" :value="item" :key="index">{{ item
+                  }}</a-select-option>
+                </a-select>
+              </a-form-item>
+              <a-form-item label="时间范围">
+                <a-range-picker v-model:value="formState.timerange" placeholder="" />
+              </a-form-item>
+            </a-form>
+          </div>
+        </div>
       </div>
-      <div class="l1">
-        <a-form :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol">
-          <!-- <a-form-item label="Activity name">
-                        <a-input v-model:value="formState.name" />
-                    </a-form-item> -->
-          <a-form-item label="页面尺寸">
-            <a-select v-model:value="formState.size" style="width: 16vh" placeholder="">
-              <a-select-option v-for="(item, index) in paperSizes" :value="item" :key="index">{{ item
-              }}</a-select-option>
-            </a-select>
-          </a-form-item>
-          <a-form-item label="缩放比例">
-            <a-select v-model:value="formState.rate" placeholder="" style="width: 16vh">
-              <a-select-option value="2">200%</a-select-option>
-              <a-select-option value="1">100%</a-select-option>
-              <a-select-option value="0.75">75%</a-select-option>
-              <a-select-option value="0.5">50%</a-select-option>
-            </a-select>
-          </a-form-item>
-          <a-form-item label="页面边距">
-            <a-input placeholder="上" style="width: 3vh" v-model:value="formState.top" />
-            <a-input placeholder="左" style="width: 3vh" v-model:value="formState.left" />
-            <a-input placeholder="下" style="width: 3vh" v-model:value="formState.bottom" />
-            <a-input placeholder="右" style="width: 3vh" v-model:value="formState.right" />
-            <span style="font-size: 1vh; color: #ffffff; margin-left: 0.6vh">
-              单位:mm</span>
-          </a-form-item>
-          <a-form-item label="页面方向">
-            <a-radio-group v-model:value="formState.direction">
-              <a-radio value="portrait">纵向</a-radio>
-              <a-radio value="landscape">横向</a-radio>
-            </a-radio-group>
-          </a-form-item>
-        </a-form>
-      </div>
-      <div class="LabelContent">
-        <span class="title-ellipsis">地图要素</span>
-      </div>
-      <div class="l2">
+
+      <!-- <div class="LabelContent" style="margin-top: 2vh">
+        <span class="title-ellipsis">{{ formState.type }}-参数设置</span>
+      </div> -->
+      <!-- <div class="l2">
         <a-form :model="formState">
-          <!-- <a-form-item label="Activity name">
-                        <a-input v-model:value="formState.name" />
-                    </a-form-item> -->
-          <a-form-item label="添加图名">
-            <a-input placeholder="请输入图名" v-model:value="formState.name" />
+          <a-form-item label="参数设置">
           </a-form-item>
-          <a-form-item label="添加副标题">
-            <a-input placeholder="请输入副标题" v-model:value="formState.subname" />
-          </a-form-item>
-          <a-form-item label="添加指北针">
-            <a-select style="width: 10vh" v-model:value="formState.north" placeholder="选择指北针">
-              <a-select-option value="2">样式一</a-select-option>
-              <a-select-option value="1">样式二</a-select-option>
-              <a-select-option value="0.75">样式三</a-select-option>
-            </a-select>
-            <a-select style="width: 10vh" v-model:value="formState.northsize" placeholder="选择尺寸">
-              <a-select-option value="2">200px</a-select-option>
-              <a-select-option value="1">100px</a-select-option>
-              <a-select-option value="0.75">50px</a-select-option>
-            </a-select>
-          </a-form-item>
-          <a-form-item label="添加比例尺">
-            <a-select style="width: 10vh" v-model:value="formState.scalebar" placeholder="选择比例尺">
-              <a-select-option value="2">样式一</a-select-option>
-              <a-select-option value="1">样式二</a-select-option>
-              <a-select-option value="0.75">样式三</a-select-option>
-            </a-select>
-            <a-select style="width: 10vh" v-model:value="formState.scalebarsize" placeholder="选择尺寸">
-              <a-select-option value="2">200px</a-select-option>
-              <a-select-option value="1">100px</a-select-option>
-              <a-select-option value="0.75">50px</a-select-option>
-            </a-select>
+          <a-form-item label="时间范围">
           </a-form-item>
         </a-form>
-      </div>
-      <div class="LabelContent">
-        <span class="title-ellipsis">出图设置</span>
-      </div>
-      <div class="l3" ref="l3">
-        <a-form :model="formState">
-          <a-form-item label="成像质量">
-            <a-select v-model:value="formState.dpi" placeholder="选择成像质量" style="width: 16vh">
-              <a-select-option value="300">300</a-select-option>
-              <a-select-option value="120">120</a-select-option>
-              <a-select-option value="100">100</a-select-option>
-              <a-select-option value="72">72</a-select-option>
-            </a-select>
-          </a-form-item>
-          <a-form-item label="地图背景色">
-            <a-input placeholder="请输入16进制颜色" v-model:value="formState.fillcolor" />
-          </a-form-item>
-          <a-form-item label="添加文件名">
-            <a-input placeholder="请输入文件名" v-model:value="formState.filename" />
-          </a-form-item>
-          <a-form-item label="导出设置">
-            <a-button size="small" style="margin-right: 1vh; font-size: 1.2vh" @click="exportImage"
-              type="primary">导出图片</a-button>
-            <a-button size="small" style="font-size: 1.2vh" @click="exportPDF" type="primary">导出PDF</a-button>
-          </a-form-item>
-        </a-form>
-        <!-- <button @click="exportImage">导出图片</button>
-                <button @click="exportPDF">导出PDF</button> -->
-      </div>
+      </div> -->
     </div>
     <div :class="['AreaSelect', leftCollapse ? 'closeLeftPanel' : 'openLeftPanel']">
       <div class="ButtonContent" @click="toolClick">
@@ -447,13 +258,64 @@ onMounted(() => {
         </div>
         <div class="container">
           <!-- <a-checkbox-group v-model:value="plainOptions" @change="changeLayer" :options="checkedList" /> -->
+          <el-checkbox-group v-model="plainOptions" @change="changeLayer">
+            <el-checkbox v-for="(item, index) in checkedList" :key="index" :label="item.label" :value="item.value">
+              {{ item.label }}
+            </el-checkbox>
+          </el-checkbox-group>
         </div>
       </div>
     </div>
   </div>
 </template>
+<style lang="less" scoped>
+:deep(.ant-select .ant-select-selector) {
+  height: 3.5vh !important;
+  background-color: #ffffff00 !important;
+  border: 1px solid #2a579ab0 !important;
+  color: aliceblue;
+  font-size: 1.2vh !important;
+}
 
+:deep(.ant-picker) {
+  height: 3.5vh !important;
+  padding: 4px 11px 4px;
+  color: rgb(251, 247, 247) !important;
+  font-size: 13px;
+  background: #ffffff00;
+  border: 1px solid #2a579ab0;
+}
+
+:deep(.ant-picker .ant-picker-input >input::placeholder) {
+  color: rgb(239 239 239) !important;
+}
+
+:deep(.ant-select .ant-select-selector .ant-select-selection-item) {
+  color: #fff !important;
+  font-size: 1.5vh !important;
+  // line-height: 2.2vh !important;
+  display: flex;
+  align-items: center;
+}
+
+:deep(.ant-picker .ant-picker-clear) {
+  background: transparent !important;
+}
+
+:deep(.ant-form-horizontal .ant-form-item-label) {
+  flex-grow: 0;
+  // width: 10vh;
+}
+
+:deep(.ant-picker .ant-picker-input >input) {
+  color: #fff !important;
+}
+</style>
 <style lang="less">
+.el-checkbox__label {
+  color: aliceblue;
+}
+
 .Mapping {
   .esri-scale-bar {
     bottom: 0;
@@ -575,8 +437,96 @@ onMounted(() => {
 }
 
 .Mapping {
+  .FlyPanel {
+    position: absolute;
+    width: 36vh;
+    top: 9vh;
+    left: 1vh;
 
-  .l1,
+    .title {
+      position: relative;
+      width: 100%;
+      height: 3.4vh;
+      line-height: 3.4vh;
+      // background: #2280cc;
+      background: url("/img/modal/header-big.svg#svgView(preserveAspectRatio(none))") no-repeat;
+      text-align: left;
+      padding-left: 2vh;
+
+      span {
+        font-size: 1.6vh;
+        font-family: puhui_SemiBold_75;
+        color: #ffffffcc;
+      }
+
+      .close {
+        width: 1.6vh;
+        height: 1.6vh;
+        position: absolute;
+        right: 1vh;
+        top: 1vh;
+        cursor: pointer;
+      }
+    }
+
+    .container {
+      // background: #07152fcc;
+      background: url("/img/modal/body-big.svg#svgView(preserveAspectRatio(none))") no-repeat;
+      margin-top: -0.2vh;
+    }
+
+    .content {
+      width: 32vh;
+      height: auto;
+      margin: 0 auto;
+      padding-top: 1vh;
+      padding-bottom: 1vh;
+
+      .LabelContent {
+        position: relative;
+        width: 29vh;
+        height: 3.24vh;
+        background: url("../assets/labelBg.png");
+        background-size: 100% 100%;
+        font-size: 1.5vh;
+        font-family: puhui_Heavy_105;
+        font-style: italic;
+        padding-left: 1.7vh;
+        color: #fff;
+        line-height: 3.24vh;
+        letter-spacing: 0.1vh;
+        display: flex;
+        margin-left: 0;
+        margin-bottom: 1.5vh;
+
+        .collapeIcon {
+          position: absolute;
+          right: -4vh;
+          top: 1vh;
+          width: 1.2vh;
+          cursor: pointer;
+          transform: rotateZ(180deg);
+
+          &.active {
+            transform: rotateZ(0deg);
+          }
+        }
+      }
+    }
+  }
+
+  .l1 {
+    height: 28vh;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    color: #fff;
+    font-size: 1.2vh;
+    align-items: center;
+    // padding: 1.5vh;
+  }
+
   .l2,
   .l3 {
     height: 25vh;
@@ -632,8 +582,8 @@ onMounted(() => {
 .AreaSelect {
   z-index: 99;
   position: absolute;
-  left: 0;
-  top: 9.8vh;
+  left: 4vh;
+  top: 8.8vh;
 
   .ButtonContent {
     position: relative;
